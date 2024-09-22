ca91c1bd0855aa9c9393496f57b1795a0d0f8313
Vladyslav <web.user@ukr.net>
2024-09-22 10:37:16 +0200

    dotenv

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
+import { config } from "dotenv";
+config();
+
 const CONFIG = {
-  // free public API, TODO: process.env
-  UNISWAP_V3_SUBGRAPH_URL:
-    "https://gateway-arbitrum.network.thegraph.com/api/338571b7d3f5b129a1b6af9e1721897d/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV",
+  UNISWAP_V3_SUBGRAPH_URL: process.env.UNISWAP_V3_SUBGRAPH_URL,
   POOL_ADDRESS: "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640",
-  START_DATE: new Date("2024-09-14T17:10:00.000Z"),
+  START_DATE: new Date("2024-01-01T01:00:00.000Z"),
   END_DATE: new Date("2024-09-15T11:15:00.000Z"),
   BATCH_SIZE: 1000,
   DELAY_BETWEEN_REQUESTS: 1000, // in milliseconds
